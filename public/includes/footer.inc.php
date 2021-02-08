35749af5a4932f2eb55ef4e0358f615a7bcfe8e9
Felix Hansmann <felix.hansmann@stud.th-luebeck.de>
2021-02-08 15:32:27 +0100

    php entfernt

--- public/includes/footer.inc.php
+++
@@ -1,9 +0,0 @@
-</main>
-<footer>
-
-</footer>
-</body>
-
-<script type="text/javascript" src="../javascript/slider.js"></script>
-<script type="text/javascript" src="../javascript/media.js"></script>
-</html>
