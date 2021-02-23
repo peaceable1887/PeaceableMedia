fd51e234b5991c8097b17d8adf32ca1e27ec5faf
Felix Hansmann <felix.hansmann@stud.th-luebeck.de>
2021-02-23 13:17:05 +0100

    slider überarbeitet, media-sektion neu gestaltet (noch test-Phase)

--- public/javascript/slider.js
+++ public/javascript/slider.js
@@ -1,29 +1,14 @@
-/*let simpleSlides = function () {
-    let i;
-    let slides = document.querySelectorAll(".slides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].setAttribute("style","display:none");
-    }
-    count++;
-    if (count > slides.length) { count = 1; }
-    slides[count-1].setAttribute("style","display:block");
-    setTimeout(simpleSlides, 5000);
-}
-
-let count = 0;
-simpleSlides();*/
-
 let slideIndex = 1;
 showSlides(slideIndex);
+
+let count = 0;
+simpleSlides();
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
+// Slider: Bildwechsel per klick
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -34,4 +19,16 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
 
+}
+// Slider: Bildwechsel in Sekundenintervallen
+function simpleSlides () {
+    let i;
+    let slides = document.querySelectorAll(".mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].setAttribute("style","display:none");
+    }
+    count++;
+    if (count > slides.length) { count = 1; }
+    slides[count-1].setAttribute("style","display:block");
+    setTimeout(simpleSlides, 7000);
 }
--- public/php/media.php
+++ public/php/media.php
@@ -1,70 +1,105 @@
-<?php
-   /* echo "<pre>";
-    print_r($_FILES);
-    echo "</pre>";
-
-    // Dateityp der zulässig ist
-    $zugelassenedateitypen = array("image/png", "image/jpeg", "image/gif");
-
-    if ( $_FILES['uploaddatei']['name']) {
-        if (!in_array($_FILES['uploaddatei']['type'], $zugelassenedateitypen)) {
-            echo "Dateitype ist NICHT zugelassen";
-        } else {
-            move_uploaded_file(
-                $_FILES['uploaddatei']['tmp_name'],
-                'hochgeladenes/' . $_FILES['uploaddatei']['name']);
-
-            echo "Hochladen war erfolgreich: ";
-            echo '<a href="hochgeladenes/' . $_FILES['uploaddatei']['name'] . '">';
-            echo 'hochgeladenes/' . $_FILES['uploaddatei']['name'];
-            echo '</a>';
-        }
-    }
-
-function clear_filename($dateiname)
-{
-    // erwünschte Zeichen erhalten bzw. umschreiben
-    // aus allen ä wird ae, ü -> ue, ß -> ss (je nach Sprache mehr Aufwand)
-    // und sonst noch ein paar Dinge (ist schätzungsweise mein persönlicher Geschmack ;)
-    $dateiname = strtolower ( $dateiname );
-    $dateiname = str_replace ('"', "-", $dateiname );
-    $dateiname = str_replace ("'", "-", $dateiname );
-    $dateiname = str_replace ("*", "-", $dateiname );
-    $dateiname = str_replace ("ß", "ss", $dateiname );
-    $dateiname = str_replace ("ß", "ss", $dateiname );
-    $dateiname = str_replace ("ä", "ae", $dateiname );
-    $dateiname = str_replace ("ä", "ae", $dateiname );
-    $dateiname = str_replace ("ö", "oe", $dateiname );
-    $dateiname = str_replace ("ö", "oe", $dateiname );
-    $dateiname = str_replace ("ü", "ue", $dateiname );
-    $dateiname = str_replace ("ü", "ue", $dateiname );
-    $dateiname = str_replace ("Ä", "ae", $dateiname );
-    $dateiname = str_replace ("Ö", "oe", $dateiname );
-    $dateiname = str_replace ("Ü", "ue", $dateiname );
-    $dateiname = htmlentities ( $dateiname );
-    $dateiname = str_replace ("&", "und", $dateiname );
-    $dateiname = str_replace (" ", "und", $dateiname );
-    $dateiname = str_replace ("(", "-", $dateiname );
-    $dateiname = str_replace (")", "-", $dateiname );
-    $dateiname = str_replace (" ", "-", $dateiname );
-    $dateiname = str_replace ("'", "-", $dateiname );
-    $dateiname = str_replace ("/", "-", $dateiname );
-    $dateiname = str_replace ("?", "-", $dateiname );
-    $dateiname = str_replace ("!", "-", $dateiname );
-    $dateiname = str_replace (":", "-", $dateiname );
-    $dateiname = str_replace (";", "-", $dateiname );
-    $dateiname = str_replace (",", "-", $dateiname );
-    $dateiname = str_replace ("--", "-", $dateiname );
-
-    $dateiname = filter_var($dateiname, FILTER_SANITIZE_URL);
-    return ($dateiname);
-}*/
-?>
+<div class="mediaManager">
+        <div class="container">
+            <img class="imageHover" src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+            <div class="overlay">
+                <div class="textHover">Bild 1 Beschreibung</div>
+            </div>
+        </div>
+        <div class="container">
+            <img class="imageHover" src="../images/Gaming-Template%20-%20Kopie-1.png" style="width:100%">
+            <div class="overlay">
+                <div class="textHover">Bild 2 Beschreibung</div>
+            </div>
+        </div>
+        <div class="container">
+            <img class="imageHover" src="../images/Flyer-Uhr.jpg" style="width:100%">
+            <div class="overlay">
+                <div class="textHover">Bild 3 Beschreibung</div>
+            </div>
+        </div>
+        <div class="container">
+            <img class="imageHover" src="../images/Visitenkarten-Farbe%20(1).jpg" style="width:100%">
+            <div class="overlay">
+                <div class="textHover">Bild 4 Beschreibung</div>
+            </div>
+        </div>
+        <div class="container">
+            <img class="imageHover" src="../images/Smartphone-Flyer%20(1).jpg" style="width:100%">
+            <div class="overlay">
+                <div class="textHover">Bild 5 Beschreibung</div>
+            </div>
+        </div>
+        <div class="container">
+            <img class="imageHover" src="../images/Parallax%20-%20Architekur-1.png" style="width:100%">
+            <div class="overlay">
+                <div class="textHover">Bild 6 Beschreibung</div>
+            </div>
+        </div>
+    <div class="container">
+        <img class="imageHover" src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+        <div class="overlay">
+            <div class="textHover">Bild 7 Beschreibung</div>
+        </div>
+    </div>
+    <div class="container">
+        <img class="imageHover" src="../images/Gaming-Template%20-%20Kopie-1.png" style="width:100%">
+        <div class="overlay">
+            <div class="textHover">Bild 8 Beschreibung</div>
+        </div>
+    </div>
+    <div class="container">
+        <img class="imageHover" src="../images/Flyer-Uhr.jpg" style="width:100%">
+        <div class="overlay">
+            <div class="textHover">Bild 9 Beschreibung</div>
+        </div>
+    </div>
+    <div class="container">
+        <img class="imageHover" src="../images/Visitenkarten-Farbe%20(1).jpg" style="width:100%">
+        <div class="overlay">
+            <div class="textHover">Bild 10 Beschreibung</div>
+        </div>
+    </div>
+    <div class="container">
+        <img class="imageHover" src="../images/Smartphone-Flyer%20(1).jpg" style="width:100%">
+        <div class="overlay">
+            <div class="textHover">Bild 11 Beschreibung</div>
+        </div>
+    </div>
+    <div class="container">
+        <img class="imageHover" src="../images/Parallax%20-%20Architekur-1.png" style="width:100%">
+        <div class="overlay">
+            <div class="textHover">Bild 12 Beschreibung</div>
+        </div>
+    </div>
+</div>
+       <!-- <img src="../images/Gaming-Template%20-%20Kopie-1.png" style="width:100%">
+        <img src="../images/Flyer-Uhr.jpg" style="width:100%">
+        <img src="../images/Visitenkarten-Farbe%20(1).jpg" style="width:100%">
+        <img src="../images/Parallax%20-%20Architekur-1.png" style="width:100%">
+        <img src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+    </div>
+    <div class="column">
+        <img src="../images/Smartphone-Flyer%20(1).jpg" style="width:100%">
+        <img src="../images/Visitenkarten-Farbe%20(1).jpg" style="width:100%">
+        <img src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+        <img src="../images/Gaming-Template%20-%20Kopie-1.png" style="width:100%">
+    </div>
+    <div class="column">
+        <img src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+        <img src="../images/Gaming-Template%20-%20Kopie-1.png" style="width:100%">
+        <img src="../images/Flyer-Uhr.jpg" style="width:100%">
+        <img src="../images/Visitenkarten-Farbe%20(1).jpg" style="width:100%">
+        <img src="../images/Parallax%20-%20Architekur-1.png" style="width:100%">
+        <img src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+    </div>
+    <div class="column">
+        <img src="../images/Smartphone-Flyer%20(1).jpg" style="width:100%">
+        <img src="../images/Visitenkarten-Farbe%20(1).jpg" style="width:100%">
+        <img src="../images/KMI-Visitenkarten-farbe.jpg" style="width:100%">
+        <img src="../images/Gaming-Template%20-%20Kopie-1.png" style="width:100%">
+    </div>
+</div>
 <ul id="galerie">
-    <li><button class="mediaContent" tabindex="1"><img src="../images/Gaming-Template%20-%20Kopie-1.png" alt="Spaß in der Sonne"></button></li>
-    <li><button class="mediaContent" tabindex="1"><img src="../images/Parallax%20-%20Architekur-1.png" alt="Berg im Morgennebel"></button></li>
-    <li><button class="mediaContent" tabindex="1"><img src="../images/Flyer-Uhr.jpg" alt="Spaß in der Sonne"></button></li>
-    <li><button class="mediaContent" tabindex="1"><img src="../images/Smartphone-Flyer%20(1).jpg" alt="Korallenriff"></button></li>
     <li><button class="mediaContent" tabindex="1"><img src="../images/KMI-Visitenkarten-farbe.jpg" alt="Morgensonne am Gardasee"></button></li>
     <li><button class="mediaContent" tabindex="1"><img src="../images/Visitenkarten-Farbe%20(1).jpg" alt="Via Appia, Rom"></button></li>
     <li><button class="mediaContent" tabindex="1"><img src="../images/Gaming-Template%20-%20Kopie-1.png" alt="Spaß in der Sonne"></button></li>
@@ -73,12 +108,4 @@ function clear_filename($dateiname)
     <li><button class="mediaContent" tabindex="1"><img src="../images/Smartphone-Flyer%20(1).jpg" alt="Korallenriff"></button></li>
     <li><button class="mediaContent" tabindex="1"><img src="../images/KMI-Visitenkarten-farbe.jpg" alt="Morgensonne am Gardasee"></button></li>
     <li><button class="mediaContent" tabindex="1"><img src="../images/Visitenkarten-Farbe%20(1).jpg" alt="Via Appia, Rom"></button></li>
-</ul>
-
-<!-- Uploadbereicht -->
-<!--<div class="uploadArea">
-    <form name="uploadformular" enctype="multipart/form-data" action="media.php" method="post">
-        Datei: <input type="file" name="uploaddatei" size="60" maxlength="255">
-        <input type="submit" name="submit" value="Datei hochladen">
-    </form>
-</div>-->
+</ul>-->
