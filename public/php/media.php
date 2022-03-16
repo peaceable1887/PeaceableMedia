c255e53b550505316cdee91a9f2c83516f4c13ca
Felix Hansmann <felix.hansmann@stud.th-luebeck.de>
2022-03-16 15:12:18 +0100

    CSS: Media-Part überarbeitet

--- public/php/media.php
+++ public/php/media.php
@@ -2,69 +2,42 @@
 <div class="hr"></div>
 <div class="mediaManager">
     <div class="container">
-        <img class="image" src="../images/Gaming-Template%20-%20Kopie-1.png">
-        <div class="overlay">
-            <div class="textHover">Gaming-Template<a class="enlargeMedia" target="_blank" href="../images/Gaming-Template%20-%20Kopie-1.png">Vergrößern</a></div>
-        </div>
+        <img class="image" src="../images/website-gf54cf3f59_1280.png">
     </div>
     <div class="containerText" >
         <h5 style="text-decoration: underline;">
             Mockups</h5>
         <p >Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
             sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br></p>
-        <br><b style="text-decoration: underline;">weitere Templates</b><br>
+        <br><b style="text-decoration: underline;">Entwürfe</b><br>
         <a href="../images/Parallax - Architekur-1.png" target="_blank">Architekur Template</a><br>
-        <a href="../images/Parallax - Architekur-1.png" target="_blank">Mode Template</a><br>
+        <a href="../images/Gaming-Template - Kopie-1.png" target="_blank">Gaming Template</a><br>
     </div>
     <div class="containerText">
         <h5 style="text-decoration: underline;">
             Corporate Design</h5>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
             sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br></p>
-        <br><b style="text-decoration: underline;">weitere Templates</b><br>
+        <br><b style="text-decoration: underline;">Entwürfe</b><br>
         <a href="../images/Visitenkarten-Farbe (1).jpg" target="_blank">Visitenkarten: TH Lübeck</a><br>
+        <a href="../images/EA2_Visitenkarten_swUndFarbe_FelixHansmann.png" target="_blank">Visitenkarten: VFG GmbH</a><br>
     </div>
     <div class="container">
-        <img class="image" src="../images/KMI-Visitenkarten-farbe.jpg">
-        <div class="overlay">
-            <div class="textHover">Visitenkarten-Design<a class="enlargeMedia" target="_blank" href="../images/KMI-Visitenkarten-farbe.jpg">Vergrößern</a></div>
-        </div>
+        <img class="image" src="../images/pencil-g1a058454d_1280.jpg">
     </div>
     <div class="container">
-        <img class="image" src="../images/Smartphone-Flyer%20(1).jpg">
-        <div class="overlay">
-            <div class="textHover">Smartphone-Flyer<a class="enlargeMedia" target="_blank" href="../images/Smartphone-Flyer%20(1).jpg">Vergrößern</a></div>
-        </div>
+        <img class="image" src="../images/email-gedee3e702_1280.png">
     </div>
     <div class="containerText">
         <h5 style="text-decoration: underline;">
             Werbung / Flyer-Design</h5>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
             sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br></p>
-        <br><b style="text-decoration: underline;">weitere Templates</b><br>
-        <a href="../images/Flyer-Uhr.jpg" target="_blank">Flyer Uhr</a><br>
-    </div>
-    <div class="containerText">
-        <h5 style="text-decoration: underline;">
-            Videos</h5>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br></p>
-        <br><b style="text-decoration: underline;">weitere Videos</b><br>
-        <a href="https://www.youtube.com/channel/UC84e0H6FkEQZHsFprSesesQ/videos" target="_blank">Link zum Channel</a><br>
-    </div>
-    <div class="container">
-        <iframe src="https://www.youtube-nocookie.com/embed/O-k9uP9hrp8" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; " allowfullscreen></iframe>
+        <br><b style="text-decoration: underline;">Entwürfe</b><br>
+        <a href="../images/EA1Uhr-Werbung-Print_FelixHansmann.png" target="_blank">Flyer Uhr</a><br>
+        <a href="../images/Smartphone-Flyer (1).jpg" target="_blank">Flyer Smartphone</a><br>
     </div>
 </div>
