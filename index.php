7f9550450b6868180ba6727bb33181710202d1d2
Felix Hansmann <felix.hansmann@stud.th-luebeck.de>
2022-05-03 18:16:53 +0200

    Tab-Icon eingefügt

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>P-Media</title>
+        <title>Peaceable-Media</title>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
+        <link rel="icon" href="images/PMedia-Logo.png">
         <link rel="stylesheet" href="css/globalColor.css" media="screen">
         <link rel="stylesheet" href="css/style.css" media="screen">
         <link rel="stylesheet" href="css/startseite.css" media="screen">
