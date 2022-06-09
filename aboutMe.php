eb926aac8cc0c5c9c3d5d4d4d75e1f1636808164
Felix Hansmann <felix.hansmann@stud.th-luebeck.de>
2022-06-09 14:37:27 +0200

    CSS-Änderungen: About Me

--- aboutMe.php
+++ aboutMe.php
@@ -1,35 +1,29 @@
-
 <div class="hr"></div>
-<div class="divManager">
+<div class="aboutMeContent">
+    <div class="aboutMeHeadline">
+        Alles, was digitalisiert werden kann, wird digitalisiert
+    </div>
+    <div class="aboutMeIcons">
+        <div class="aboutMeIconsWrapper">
+            <img class="icon" src="images/iconmonstr-code-2-240.png">
+            <h5>Coding</h5>
+        </div>
+        <div class="aboutMeIconsWrapper" >
+            <img class="icon" style="margin-left: 10px;" src="images/iconmonstr-pen-18-240.png">
+            <h5>Design</h5>
+        </div>
+        <div class="aboutMeIconsWrapper">
+            <img class="icon" src="images/iconmonstr-video-15-240.png">
+            <h5>Video</h5>
+        </div>
+    </div>
+    <div class="aboutMeText">
+        <p><b>Über Mich</b><br><br>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><br>
+            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
 
-<div class="textAboutMe">
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-    At vero eos et accusam et justo duo dolores et ea rebum.<br>
-    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    <br>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-        At vero eos et accusam et justo duo dolores et ea rebum.<br>
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    <br>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br>
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
-        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-        At vero eos et accusam et justo duo dolores et ea rebum.<br>
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    <br>
-
-</div>
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+    </div>
 </div>
