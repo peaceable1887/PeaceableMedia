4c922be29ab608bfbf745218fbb09f53f42c0113
Felix Hansmann <felix.hansmann@stud.th-luebeck.de>
2022-06-16 12:02:59 +0200

    Media: Links zu Dropbox eingefügt

--- media.php
+++ media.php
@@ -9,7 +9,7 @@
                 <h5>Werbung / Flyer-Design</h5>
                 <p >Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><a href="">mehr...</a></p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><a href="https://www.dropbox.com/sh/jgqubfau4qbfkhr/AAAZ3PBKvXckppc1Aa1NSUGFa?dl=0">mehr...</a></p>
                 <!-- <br><b style="text-decoration: underline;">Entwürfe</b><br>
                 <a href="images/Parallax%20-%20Architekur-1.png" target="_blank">Architekur Template</a><br>
                <a href="images/Gaming-Template%20-%20Kopie-1.png" target="_blank">Gaming Template</a><br>-->
@@ -22,7 +22,7 @@
                 <h5>Corporate Design</h5>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><a href="">mehr...</a></p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><a href="https://www.dropbox.com/sh/5c9bf99avvdpcd9/AAAoqIRTGtKOB7Acs-Qq8ohLa?dl=0">mehr...</a></p>
                 <!--<br><b style="text-decoration: underline;">Entwürfe</b><br>
                 <a href="images/Visitenkarten-Farbe%20(1).jpg" target="_blank">Visitenkarten: TH Lübeck</a><br>
                 <a href="images/EA2_Visitenkarten_swUndFarbe_FelixHansmann.png" target="_blank">Visitenkarten: VFG GmbH</a><br>-->
@@ -41,7 +41,7 @@
                 <h5>Mockup Konzeption</h5>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br>
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><a href="">mehr...</a></p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br><a href="https://www.dropbox.com/sh/scn9h50qy36934p/AACyfcjiCEXF0XpfIK4LR5Mna?dl=0">mehr...</a></p>
                 <!-- <br><b style="text-decoration: underline;">Entwürfe</b><br>
                  <a href="images/EA1Uhr-Werbung-Print_FelixHansmann.png" target="_blank">Flyer Uhr</a><br>
                  <a href="images/Smartphone-Flyer%20(1).jpg" target="_blank">Flyer Smartphone</a><br>-->
