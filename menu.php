a95322875cf7e40b5c439614db8480bbb8177052
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-08 17:04:10 +0100

    update nav 2.0

--- menu.php
+++ menu.php
@@ -4,16 +4,16 @@
     if ($_SESSION['changedPage']) 
     {
         echo '
-        <a href="home.php">
+        <a href="https://www.felix-hansmann.de/">
             <div id="logo">
                 <span id="first-name">Felix</span>
                 <span id="last-name">Hansmann</span>
             </div>
         </a>
         <nav id="navItems">
-        <a class="navLink" href="home.php" data-scroll="home">Home</a>
-        <a class="navLink" href="projects.php" data-scroll="projects">Projects</a>
-        <a class="navLink" href="footer.php" data-scroll="contact">Contact</a>';
+        <a class="navLink" href="https://www.felix-hansmann.de/#projects" data-scroll="home">Home</a>
+        <a class="navLink" href="https://www.felix-hansmann.de/#aboutMe" data-scroll="projects">Projects</a>
+        <a class="navLink" href="https://www.felix-hansmann.de/#contact" data-scroll="contact">Contact</a>';
       
     } else 
     {
