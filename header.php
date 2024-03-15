3897b6003f5236b1dac023f70d0e4586c1a2d9f3
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-15 14:55:52 +0100

    updated home.css

--- header.php
+++ header.php
@@ -20,7 +20,6 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-        <link rel="icon" href="images/PMedia-Logo.png">
         <link rel="stylesheet" href="css/globalColor.css" media="screen">
         <link rel="stylesheet" href="css/style.css" media="screen">
         <link rel="stylesheet" href="css/slider.css" media="screen">
