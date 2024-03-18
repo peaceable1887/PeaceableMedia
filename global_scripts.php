b2742487520b282d49521f5370693095c9e7ac44
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-18 16:43:16 +0100

    changed animation duration from the slider

--- global_scripts.php
+++ global_scripts.php
@@ -1,5 +1,5 @@
 </body>
-    <!--<script type="text/javascript" src="javascript/typewriter.js"></script>-->
+    <script type="text/javascript" src="javascript/testScrollEvents.js"></script>
     <script type="text/javascript" src="javascript/resizeScrollFunctions.js"></script>
     <script type="text/javascript" src="javascript/burgerMenu.js"></script>
     <script type="text/javascript" src="javascript/coveringContentOnScroll.js"></script>
