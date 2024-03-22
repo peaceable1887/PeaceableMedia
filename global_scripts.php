6a6abc3fb04730a0106ccd209c29c3eb4b38d7e9
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-22 12:15:17 +0100

    removed coveringContentOnScroll.js

--- global_scripts.php
+++ global_scripts.php
@@ -2,5 +2,5 @@
     <script type="text/javascript" src="javascript/menuScrollEvents.js"></script>
     <script type="text/javascript" src="javascript/menuResizeEvents.js"></script>
     <script type="text/javascript" src="javascript/burgerMenu.js"></script>
-    <script type="text/javascript" src="javascript/coveringContentOnScroll.js"></script>
+
 </html>
