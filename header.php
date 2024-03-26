c11f5711f3dd1260865fd025cdf5a12b7566f353
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-26 16:00:33 +0100

    added favicon icon for google

--- header.php
+++ header.php
@@ -16,7 +16,8 @@
         sich in Entwicklung befindende Projekte sowie Designkonzeptionen für Webseiten, mobile Anwendungen und Flyer.">
         <meta name="author" content="Felix Hansmann">
         <title>Felix Hansmann - Entwickler und Designer</title>
-        <link rel="icon" type="image/x-icon" href="images/Logo_32x32.png" sizes="32x32">
+        <link rel="icon" type="image/x-icon" href="images/Logo_32x32.png" >
+        <link rel="icon" type="image/png" href="images/Logo_48x48.png" >
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
