fc8bea3d65fdac2e5cbfbf20de97410b96948208
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-27 10:59:14 +0100

    added size parameter to the link tag

--- header.php
+++ header.php
@@ -16,9 +16,9 @@
         sich in Entwicklung befindende Projekte sowie Designkonzeptionen für Webseiten, mobile Anwendungen und Flyer.">
         <meta name="author" content="Felix Hansmann">
         <title>Felix Hansmann - Entwickler und Designer</title>
-        <link rel="icon" type="image/x-icon" href="images/Logo_32x32.png" >
-        <link rel="icon" type="image/png" href="images/Logo_48x48.png" >
-        <link rel="icon" type="image/png" href="images/Logo_192x192.png" >
+        <link rel="icon" type="image/x-icon" href="images/Logo_32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="images/Logo_48x48.png" sizes="48x48">
+        <link rel="icon" type="image/png" href="images/Logo_192x192.png" sizes="192x192">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
