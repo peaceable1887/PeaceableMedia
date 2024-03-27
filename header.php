91abda2a7133c8bfd59ece38c8aa296d0401f6fc
Felix Hansmann <felix-hh1887@hotmail.de>
2024-03-27 10:56:39 +0100

    added logo with the size of 192x192 px

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
         <title>Felix Hansmann - Entwickler und Designer</title>
         <link rel="icon" type="image/x-icon" href="images/Logo_32x32.png" >
         <link rel="icon" type="image/png" href="images/Logo_48x48.png" >
+        <link rel="icon" type="image/png" href="images/Logo_192x192.png" >
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
